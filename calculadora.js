1aaa3f0bcf472d4cd40a993353f6d07c546aeb00
RCRecalde <rorecalde3@gmail.com>
2023-02-25 18:24:06 -0300

    primer commit

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+const sumar = require('./Cuentitas/sumar');
+const restar = require('./Cuentitas/restar');
+const multiplicar = require('./Cuentitas/multiplicar');
+const dividir = require('./Cuentitas/dividir');
+
+//console.log(sumar(99, 87));
+
+//console.log(restar(20, 16));
+
+console.log(multiplicar(2, 2));
+//console.log(multiplicar(21, 0));
+
+//console.log(dividir(5, 2));
+//console.log(dividir(6, 0));
